f71dbc3d429742fe28286f4ebde0975aa88617cb
sharmap4 <sharmapulkitmukesh@gmail.com>
2021-02-19 15:29:29 +0000

    back to silvia

--- room-1-hackathon/src/components/PokemonController/index.js
+++ room-1-hackathon/src/components/PokemonController/index.js
@@ -1,24 +1,21 @@
 import React from "react";
 
-function PokemonController({ reducer, text, updateText, searchById }) {
-  return (
-    <div>
-      <div>
-        <button onClick={reducer({ type: "randomSearch" })}>
-          Get Random Pokemon
-        </button>
-      </div>
+function PokemonController({handleClick, text, updateText, searchByIdClick}) {
 
-      <div>
-        <input
-          onChange={(event) => {
-            updateText(event);
-          }}
-        />
-        <button onClick={reducer({ type: "searchById" })}>Search by id</button>
-      </div>
+
+return(
+<div>
+    <div>
+    <button onClick={handleClick}>Get Random Pokemon</button>
     </div>
-  );
+    <div>
+    <input onChange={(event) => {updateText(event)}} />
+    <button onClick={searchByIdClick}>Search by id</button>
+    </div>
+</div>
+)
+
+
 }
 
 export default PokemonController;
