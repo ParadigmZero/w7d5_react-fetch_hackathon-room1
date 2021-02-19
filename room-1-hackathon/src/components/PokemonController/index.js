d67d92416cd0c6b16b5721431e7cad1b969b1c47
ParadigmZero <josephrcalton@gmail.com>
2021-02-19 10:23:09 +0000

    Laying down initial structure and ideas

---
+++ room-1-hackathon/src/components/PokemonController/index.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+function PokemonController() {
+
+}
+
+export default PokemonController;
+
+// {/* TODO: call handleClick when button clicked */ }
+// <button onClick={handleClick}>Get Random Pokemon</button>
+// {/* TODO: hand down id as a prop */ }
+
+
+// function handleClick() {
+//     // TODO: Set id to be random number between 1 and 151
+//     setId(Math.floor(Math.random() * 151) + 1);
+// }
