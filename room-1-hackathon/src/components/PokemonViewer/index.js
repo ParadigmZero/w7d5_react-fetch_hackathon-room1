b0bfde668163c69c4b7c49f26b02ae91f5dd9258
SilviaEPopescu <s.emilia.popescu@gmail.com>
2021-02-19 11:26:25 +0000

    table

--- room-1-hackathon/src/components/PokemonViewer/index.js
+++ room-1-hackathon/src/components/PokemonViewer/index.js
@@ -12,21 +12,49 @@ function PokemonViewer({ id, pokemon, setPokemon }) {
             );
             let data = await response.json();
             setPokemon(data);
-            console.log(process.env.REACT_APP_API_URL);
-            console.log(pokemon);
+           
+           // console.log(pokemon);
         }
         getPokemon();
     }, [id]);
 
+    console.log(pokemon.stats && pokemon.stats[0].base_stat);
+    console.log(pokemon);
     return (
         <div className="pokemon-viewer">
             <p>{pokemon.name}!</p>
-            <p>{pokemon.height}!</p>
-            < img src={pokemon.sprites?.front_default} alt="front" />
-            < img src={pokemon.sprites?.back_default} alt='back' />
-            <ul>
 
-            </ul>
+            <div>
+                   < img src={pokemon.sprites?.front_default} alt="front" />
+                   < img src={pokemon.sprites?.back_default} alt='back' />
+            </div>
+            
+            <table id="playerTable">
+                    <tr>
+                        <td>HP</td>
+                        <td id="playerHp"> {pokemon.stats && pokemon.stats[0].base_stat}</td>
+                    </tr>
+                    <tr>
+                        <td>Attack</td>
+                        <td id="playerAttack"> {pokemon.stats && pokemon.stats[1].base_stat}</td>
+                    </tr>
+                    <tr>
+                        <td>Defense</td>
+                        <td id="playerDefense"> {pokemon.stats && pokemon.stats[2].base_stat}</td>
+                    </tr>
+                    <tr>
+                        <td>Special Attack</td>
+                        <td id="playerSpecialAttack"> {pokemon.stats && pokemon.stats[3].base_stat}</td>
+                    </tr>
+                    <tr>
+                        <td>Special Defense</td>
+                        <td id="playerSpecialDefense"> {pokemon.stats && pokemon.stats[4].base_stat}</td>
+                    </tr>
+                    <tr>
+                        <td>Speed</td>
+                        <td id="playerSpeed"> {pokemon.stats && pokemon.stats[5].base_stat}</td>
+                    </tr>
+                </table>
 
         </div>
     );
