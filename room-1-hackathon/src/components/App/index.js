033f9b2942d1303a337ea66e9df01240ddad5d2d
sharmap4 <sharmapulkitmukesh@gmail.com>
2021-02-19 15:30:53 +0000

    back to silvia

--- room-1-hackathon/src/components/App/index.js
+++ room-1-hackathon/src/components/App/index.js
@@ -3,36 +3,36 @@ import PokemonController from "../PokemonController";
 import PokemonViewer from "../PokemonViewer";
 
 function App() {
-
   const [id, setId] = useState(Math.floor(Math.random() * 151) + 1);
   const [pokemon, setPokemon] = useState("");
 
-  const[text, setText] = useState('');
+  const [text, setText] = useState("");
 
-  function updateText(event){
-      setText(event.target.value);
+  function updateText(event) {
+    setText(event.target.value);
   }
 
-  function searchByIdClick(){
-    if(text>1 && text<150){
-      setId(text)
+  function searchByIdClick() {
+    if (text > 1 && text < 150) {
+      setId(text);
     }
-      
   }
-
-
 
   function handleClick() {
     // TODO: Set id to be random number between 1 and 151
     setId(Math.floor(Math.random() * 151) + 1);
   }
-
+  //s
 
   return (
     <div className="App">
       <PokemonViewer id={id} pokemon={pokemon} setPokemon={setPokemon} />
-      <PokemonController text={text} updateText={updateText} searchByIdClick={searchByIdClick} handleClick={handleClick}/>
-      
+      <PokemonController
+        text={text}
+        updateText={updateText}
+        searchByIdClick={searchByIdClick}
+        handleClick={handleClick}
+      />
     </div>
   );
 }
